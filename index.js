84cfef3dd847b8c28341f397c7683b21e42305cf
lola <slonanuga@gmail.com>
2018-11-22 20:32:24 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,53 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
+
 let question;
+let questionbox = document.querySelector(".question-container")
+
+
+questionbox.appendChild(que);
+
+function appendQuestion (question) {
+    
+    questionbox.innerText = question['questionText']
+    }
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(question)}
+    ,time)
+})
+}
+    
+function removeQuestion() {
+  return new Promise(function (resolve) {
+  questionbox.innerText = ''})
+}
+
+function askQuestionThenRemoveQuestion (time) {
+return askQuestionThen(time).then(removeQuestion)
+
+}
+
+function trueAndFalseButtons() {
+return document.querySelector('.true-false-list').querySelectorAll('.btn')
+
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick() {
+  let btn = document.querySelector('a')
+  btn.addEventListener('click', function () {
+    toggleTrueAndFalseButtons() 
+    askQuestionThenRemoveQuestion (5000)
+  })
+}
