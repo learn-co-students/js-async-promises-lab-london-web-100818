de7b818193f8609dd18cde4b8cdb5fc66ab0f403
natashabuck <natashabuck1@gmail.com>
2018-11-24 16:12:13 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const questions = [
 ]
 
 let question;
+let promise = new Promise(function(resolve){});
+const questionContainer = document.querySelector(".question-container");
+const buttons = document.querySelector('.true-false-list').querySelectorAll('.btn')
+
+function appendQuestion(question) {
+  questionContainer.innerHTML = question.questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  for (const q of questions) {
+    if(q.questionText === questionContainer.innerText) {
+      question = q;
+    }
+  }
+  return new Promise((resolve) => {
+    setTimeout(function(){
+      resolve()
+    }, time )
+  })
+}
+
+function removeQuestion() {
+  return new Promise(function(){
+    questionContainer.innerHTML = ''
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion); 
+}
+
+function trueAndFalseButtons() {
+  return buttons;
+}
+
+function toggleTrueAndFalseButtons() {
+  for (const b of buttons) {
+    if (b.classList.contains('hide')) { b.classList.remove('hide');
+    } else {
+      b.classList.add('hide')
+    }
+  }
+}
+
+function displayQuestionOnClick() {
+  
+}
